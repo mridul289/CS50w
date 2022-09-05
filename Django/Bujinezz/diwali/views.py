2e9da14cea213cf6eee942aa5afb495a85d78bb1
mridul289 <65486499+mridul289@users.noreply.github.com>
2022-09-05 18:26:38 +0530

    Diwali

---
+++ Django/Bujinezz/diwali/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+import datetime
+# Create your views here.
+
+def index(request):
+    now = datetime.datetime.now()
+    return render(request, 'index.html', {
+        "here": now.month == 10 
+        })
